d51a1b327a43ad23f2ef775c732e25eb84a7991b
sekiguchi0215 <dsphere5296@gmail.com>
2021-08-17 00:53:16 +0900

    初期データを導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user_params = [
+  {
+    email: "example@example.com",
+    password: "password",
+  },
+]
+
+User.delete_all
+User.create!(user_params)
+puts "ユーザーの初期データの投入に成功しました。"
+
+post_params = [
+  {
+    user_id: 1,
+    title: "React",
+    content: "ユーザインターフェース構築のための JavaScript ライブラリ",
+  },
+  {
+    user_id: 1,
+    title: "Vue.js",
+    content: "The Progressive JavaScript Framework",
+  },
+  {
+    user_id: 1,
+    title: "Angular",
+    content: "モバイルとデスクトップ，ひとつのフレームワーク",
+  },
+]
+
+Post.delete_all
+Post.create!(post_params)
+puts "Postの初期データの投入に成功しました。"
+
+puts "すべての初期データ投入に成功しました。"
